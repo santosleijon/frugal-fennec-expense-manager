a773c1faa848ad583a1f35cec1f7adc97792ad48
Santos Leijon <santos.bostrom.leijon@gmail.com>
2022-02-06 15:23:12 +0100

    Make expenses uneditable in DataGrid

--- ui/src/components/App/Main/Expenses/ExpensesDataGrid/index.tsx
+++ ui/src/components/App/Main/Expenses/ExpensesDataGrid/index.tsx
@@ -1,6 +1,5 @@
-import { Button, Card, CardContent, FormControl, FormHelperText, MenuItem } from "@material-ui/core";
-import Select, { SelectChangeEvent } from '@mui/material/Select';
-import { DataGrid, GridColDef, GridRenderCellParams, GridRowId, GridRowsProp } from '@mui/x-data-grid';
+import { Button, Card, CardContent } from "@material-ui/core";
+import { DataGrid, GridColDef, GridRowId, GridRowsProp } from '@mui/x-data-grid';
 import DataGridContainer from "components/App/common/DataGridContainer";
 import * as React from 'react';
 import { useState } from "react";
@@ -15,68 +14,29 @@ interface ExpensesDataGridProps {
 }
 
 export default function ExpensesDataGrid(props: ExpensesDataGridProps) {
-  function AccountEditCell(props: GridRenderCellParams<number> & { accounts: Account[] }): JSX.Element {
-    const onChange = (event: SelectChangeEvent) => {
-      const newSelectedAccountId = +event.target.value
-      console.log("newSelectedAccountId=", newSelectedAccountId)
-    }
-
-    const selectedAccountId = props.accounts.find((a: Account) => a.name === props.row.account)?.id
-  
-    return (
-      <FormControl variant="filled">
-        <Select
-          id="account-field-{selectedAccountId}"
-          displayEmpty
-          inputProps={{ 'aria-label': 'Account' }}
-          value={selectedAccountId}
-          variant="outlined"
-          onChange={onChange}
-          className="inputField"
-        >
-          {props.accounts.map((account) => (
-            <MenuItem key={account.id} value={account.id}>
-              {account.name}
-            </MenuItem>
-          ))}
-        </Select>
-        <FormHelperText>Account</FormHelperText>
-      </FormControl>
-    );
-  }
-
-  function renderAccountEditCell(params: any) {
-    return <AccountEditCell {...params} accounts={props.accounts} />;
-  }
-
   const columns: GridColDef[] = [
     {
       field: 'date',
       headerName: 'Date',
       width: 200,
       type: 'date',
-      editable: true,
     },
     {
       field: 'account',
       headerName: 'Account',
       width: 200,
-      editable: true,
-      renderEditCell: renderAccountEditCell,
     },
     {
       field: 'description',
       headerName: 'Description',
       width: 250,
       type: 'string',
-      editable: true,
     },
     {
       field: 'amount',
       headerName: 'Amount',
       width: 200,
       type: 'number',
-      editable: true,
     },
   ];
 
