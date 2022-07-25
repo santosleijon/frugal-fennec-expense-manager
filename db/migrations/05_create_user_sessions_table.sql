154ca298d95d37c7ed86f21c16114a6f2ec67496
Santos Leijon <santos.bostrom.leijon@gmail.com>
2022-07-25 13:06:37 +0200

    Allow multiple user sessions per user

--- db/migrations/05_create_user_sessions_table.sql
+++ db/migrations/05_create_user_sessions_table.sql
@@ -1,9 +1,10 @@
 \c frugal_fennec;
 CREATE TABLE IF NOT EXISTS user_sessions (
-  user_id UUID PRIMARY KEY,
+  user_id UUID NOT NULL,
   token VARCHAR(512) NOT NULL,
   issued TIMESTAMP WITH TIME ZONE NOT NULL,
   valid_to TIMESTAMP WITH TIME ZONE NOT NULL
 );
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE user_sessions TO frugal_fennec;
+CREATE INDEX user_sessions_user_id_idx ON user_sessions(user_id);
 CREATE UNIQUE INDEX user_sessions_token_idx ON user_sessions(token);
