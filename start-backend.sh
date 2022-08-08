bc44959557a8bb939e47add0badc5e3fe1893788
Santos Leijon <santos.bostrom.leijon@gmail.com>
2022-08-08 19:46:00 +0200

    Support multiple environment variables in backend start script

--- start-backend.sh
+++ start-backend.sh
@@ -1,1 +1,5 @@
-sudo docker start frugal-fennec_db_1 && java -jar `ls ./target/frugal-fennec-expense-manager-*.jar`
+for env in *.env; do
+  source "$env" 
+done
+
+sudo docker start frugal-fennec-expense-manager_db_1 && java -jar `ls ./target/frugal-fennec-expense-manager-*.jar`
